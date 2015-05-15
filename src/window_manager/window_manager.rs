c71fda1b17c016bdd1cd23c49243a98f2efd1581
lutianming <lutianming1005@gmail.com>
2015-05-15 18:59:26 +0200

    add get_window_property fn

--- src/window_manager/window_manager.rs
+++ src/window_manager/window_manager.rs
@@ -28,6 +28,42 @@ fn get_text_property(display: *mut xlib::Display, window: xlib::Window, atom: xl
     }
 }
 
+fn get_window_property(display: *mut xlib::Display, window: xlib::Window, atom: xlib::Atom) {
+    unsafe {
+        let mut actual_type_return: u64 = 0;
+        let mut actual_format_return: i32 = 0;
+        let mut nitem_return: libc::c_ulong = 0;
+        let mut bytes_after_return: libc::c_ulong = 0;
+        let mut prop_return: *mut libc::c_uchar = mem::zeroed();
+
+        let r = xlib::XGetWindowProperty(display, window, xlib::XA_WM_NAME,
+                                         0, 0xFFFFFFFF, 0, 0,
+                                         &mut actual_type_return,
+                                         &mut actual_format_return,
+                                         &mut nitem_return,
+                                         &mut bytes_after_return,
+                                         &mut prop_return);
+
+        debug!("result get wp {}", r);
+        if r == 0 {
+
+        }else{
+            debug!("actual format return {}", actual_format_return);
+            if actual_format_return == 0 {
+
+            }
+            else {
+                // let s = from_raw_parts(prop_return as *const libc::c_ulong, nitems_return as usize).iter()
+                //     .map(|&c| c as u64)
+                //     .collect();
+                let s = String::from_raw_parts(prop_return, nitem_return as usize, nitem_return as usize + 1 );
+                println!("{}", s);
+
+            }
+        }
+    }
+}
+
 pub struct WindowManager {
     pub display: *mut xlib::Display,
     pub screen_num: libc::c_int,
@@ -108,41 +144,6 @@ impl WindowManager {
                             Some(s) => {debug!("create window {} for {}", event.window, s);}
                             None => {}
                         }
-
-                        // let mut actual_type_return: u64 = 0;
-                        // let mut actual_format_return: i32 = 0;
-                        // let mut nitem_return: libc::c_ulong = 0;
-                        // let mut bytes_after_return: libc::c_ulong = 0;
-                        // let mut prop_return: *mut libc::c_uchar = mem::zeroed();
-                        // let r = xlib::XGetWindowProperty(self.display, event.window, xlib::XA_WM_NAME,
-                        //                          0, 0xFFFFFFFF, 0, 0,
-                        //                          &mut actual_type_return,
-                        //                          &mut actual_format_return,
-                        //                          &mut nitem_return,
-                        //                          &mut bytes_after_return,
-                        //                          &mut prop_return);
-
-                        // debug!("result get wp {}", r);
-                        // if r == 0 {
-
-                        // }else{
-                        //     debug!("actual format return {}", actual_format_return);
-                        //     if actual_format_return == 0 {
-
-                        //     }
-                        //     else {
-                        //         // let s = from_raw_parts(prop_return as *const libc::c_ulong, nitems_return as usize).iter()
-                        //         //     .map(|&c| c as u64)
-                        //         //     .collect();
-                        //         let s = String::from_raw_parts(prop_return, nitem_return as usize, nitem_return as usize + 1 );
-                        //         println!("{}", s);
-
-                        //     }
-                        // }
-
-
-
-
                     }
                     xlib::DestroyNotify => {
                         debug!("destroy notify");
